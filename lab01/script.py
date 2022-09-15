f5384b77966e63e362e604d448a66eeff8e8dd3d
eojo459 <emmanuelojo025@gmail.com>
2022-09-14 21:39:31 -0600

    modify script to download itself and print source

--- lab01/script.py
+++ lab01/script.py
@@ -1,2 +1,3 @@
 import requests
-r = requests.get('https://google.com')
+r = requests.get('https://github.com/eojo459/404-course-not-found/blob/master/lab01/script.py')
+open('script_downloaded.py', 'wb').write(r.content)
